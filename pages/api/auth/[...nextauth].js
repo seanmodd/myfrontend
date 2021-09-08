9493fd1daa986454d83c612fb2e7bb954a5ba074
Sean <sean@vestaridefair.com>
2021-09-07 18:57:43 -0700

    OK

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,27 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+// import NextAuth from "next-auth"
+// import Providers from "next-auth/providers"
+// import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+// import clientPromise from "lib/mongodb"
 
+// export default async function auth(req, res) {
+//   return await NextAuth(req, res, {
+
+//     adapter: MongoDBAdapter({
+//       db: (await clientPromise).db(process.env.MONGODB_URI)
+
+//     }),
+
+//   })
+// }
+
+import NextAuth from 'next-auth';
+
+import Providers from 'next-auth/providers';
 import { connectToDatabase } from '../../../lib/db/mongodb';
 import { verifyPassword } from '../../../lib/auth/auth';
+// import { FirebaseAdapter } from "@next-auth/firebase-adapter"
+// import firebase from "firebase/app"
+// import "firebase/firestore"
 
 const options = {
   session: {
@@ -37,56 +56,56 @@ const options = {
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      async authorize(credentials) {
-        try {
-          const { db } = await connectToDatabase();
-          const user = await db.collection('users-permissions_user').findOne({
-            email: credentials.email,
-          });
+      // async authorize(credentials) {
+      //   try {
+      //     const { db } = await connectToDatabase();
+      //     const user = await db.collection('users-permissions_user').findOne({
+      //       email: credentials.email,
+      //     });
 
-          if (!user) throw new Error('No user found');
+      //     if (!user) throw new Error('No user found');
 
-          const isPasswordValid = await verifyPassword(
-            credentials.password,
-            user.password
-          );
+      //     const isPasswordValid = await verifyPassword(
+      //       credentials.password,
+      //       user.password
+      //     );
 
-          if (!isPasswordValid) throw new Error('Password is not valid');
+      //     if (!isPasswordValid) throw new Error('Password is not valid');
 
-          return {
-            email: user.email,
-          };
-        } catch (error) {
-          throw new Error(error);
-        }
-      },
+      //     return {
+      //       email: user.email,
+      //     };
+      //   } catch (error) {
+      //     throw new Error(error);
+      //   }
+      // },
     }),
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      async authorize(credentials) {
-        try {
-          const { db } = await connectToDatabase();
-          const user = await db.collection('users-permissions_user').findOne({
-            email: credentials.email,
-          });
+      // async authorize(credentials) {
+      //   try {
+      //     const { db } = await connectToDatabase();
+      //     const user = await db.collection('users-permissions_user').findOne({
+      //       email: credentials.email,
+      //     });
 
-          if (!user) throw new Error('No user found');
+      //     if (!user) throw new Error('No user found');
 
-          const isPasswordValid = await verifyPassword(
-            credentials.password,
-            user.password
-          );
+      //     const isPasswordValid = await verifyPassword(
+      //       credentials.password,
+      //       user.password
+      //     );
 
-          if (!isPasswordValid) throw new Error('Password is not valid');
+      //     if (!isPasswordValid) throw new Error('Password is not valid');
 
-          return {
-            email: user.email,
-          };
-        } catch (error) {
-          throw new Error(error);
-        }
-      },
+      //     return {
+      //       email: user.email,
+      //     };
+      //   } catch (error) {
+      //     throw new Error(error);
+      //   }
+      // },
     }),
   ],
   pages: {
---
+++ store/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSu7NJhrQMFggOUbygUmMqx_2b_AHBb6g",
+  authDomain: "strapiauth-ad816.firebaseapp.com",
+  databaseURL: "https://strapiauth-ad816-default-rtdb.firebaseio.com",
+  projectId: "strapiauth-ad816",
+  storageBucket: "strapiauth-ad816.appspot.com",
+  messagingSenderId: "857658107134",
+  appId: "1:857658107134:web:5c3c0ab9698d598808f1af"
+};
